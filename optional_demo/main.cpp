a255d9dfedc6264910ba78809c81b19cead107a1
abin <abin.z@outlook.com>
2025-01-10 14:25:45 +0800

    feat: 修改optionalDemo

--- optional_demo/main.cpp
+++ optional_demo/main.cpp
@@ -11,13 +11,19 @@ struct Object
 
 int main()
 {
-    fmt::println("hello optional.");
-    std::bitset<1> bset;
-    fmt::println("std::bitset<1> size = {}", sizeof(bset));
+    fmt::println("----------bitset----------");
+    std::bitset<8> bset(0b11001100);
+    fmt::println("Bitset as ulong: {}", bset.to_ulong());
+    fmt::println("Bitset as string: {}", bset.to_string());
+    bset.set(0); // 从右往左,将第0位置为1 
+    fmt::println("bset.set(0) after: {}", bset.to_string());
+    bset.reset(7); // 从右往左,将第7位置为0 
+    fmt::println("bset.reset(7) after: {}", bset.to_string());
 
+    fmt::println("----------optional----------");
     // Case 1: Optional without value (default)
     std::optional<Object> o1;
-    if (o1)
+    if (o1.has_value())
     {
         fmt::println("o1 has value");
     }
@@ -37,9 +43,14 @@ int main()
     if (o2)
     {
         fmt::println("o2 has value");
+        fmt::println("*(o2->ptr) = {}, o2->n = {}", *(o2->ptr), o2->n);
     }
     else
     {
         fmt::println("o2 doesn't have value");
     }
+
+    // 使用 value_or 来提供默认值
+    std::optional<int> opt;
+    fmt::println("opt value = {}", opt.value_or(999)); // 输出默认值999
 }
